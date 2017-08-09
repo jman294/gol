eafc3c02e18b69b542d06359f72ddff05c249ef1
Jack DuPlessis <jduplessis294@gmail.com>
2017-08-09 11:05:29 -0400

    Add bounding box for visuals

--- gol.py
+++ gol.py
@@ -2,24 +2,22 @@ import time
 
 def neighbors (point):
     x, y = point
-    return [(x, y+1),
-            (x, y-1),
-            (x+1, y),
-            (x-1, y),
-            (x+1, y+1),
-            (x-1, y+1),
-            (x+1, y-1),
-            (x-1, y-1)]
+    yield (x, y+1)
+    yield (x, y-1)
+    yield (x+1, y)
+    yield (x-1, y)
+    yield (x+1, y+1)
+    yield (x-1, y+1)
+    yield (x+1, y-1)
+    yield (x-1, y-1)
 
 def advance (board):
     new = set([])
     for point in board:
-        nbrs = neighbors(point)
         num = 0
-        for neighbor in nbrs:
-            sneighbors = neighbors(neighbor)
+        for neighbor in neighbors(point):
             snum = 0
-            for sneighbor in sneighbors:
+            for sneighbor in neighbors(neighbor):
                 if sneighbor in board:
                     snum += 1
             if snum == 3 and neighbor:
@@ -28,20 +26,39 @@ def advance (board):
                 num += 1
         if num == 2 or num == 3:
             new.add(point)
-    return list(new)
+    return new
 
-# board = [(0, 0), (1, -1), (1, -2), (0, -2), (-1, -2)]
-# board = [(0, 0), (1, 0), (0, -1), (1, -1)]
-# board = [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)]
-# board = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)]
-board = [(0, 0), (0, 2), (3, 0), (1, 3), (2, 3), (3, 3), (4, 3), (4, 2), (4, 1)]
+# board = set([(0, 0), (1, -1), (1, -2), (0, -2), (-1, -2)])
+# board = set([(0, 0), (1, 0), (0, -1), (1, -1)])
+# board = set([(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)])
+# board = set([(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)])
+# board = set([(0, 0), (0, 2), (3, 0), (1, 3), (2, 3), (3, 3), (4, 3), (4, 2), (4, 1)])
 while True:
-    for j in range(-20, 20):
-        for i in range(-50, 50):
-            if (i, j) in board:
-                print('0', end='')
+    first = board.pop()
+    board.add(first)
+    ax, ay = first
+    bx, by = first
+    for point in board:
+        x, y = point
+        if x > ax:
+            ax = x
+        if y > ay:
+            ay = y
+        if x < bx:
+            bx = x
+        if y < by:
+            by = y
+    print(bx, ay, ax, by)
+
+    for y in range(ay, by-1, -1):
+        for x in range(bx, ax+1):
+            if (x, y) in board:
+                print('#', end='')
+            elif (0, 0) == (x, y):
+                print('G', end='')
             else:
-                print('.', end='')
+                print('"', end='')
         print('')
     board = advance(board)
-    time.sleep(.2)
+    # time.sleep(.2)
+    input()
